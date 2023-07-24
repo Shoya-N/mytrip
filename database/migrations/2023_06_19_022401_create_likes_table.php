102dc0349d69e0c268170effe69eb088e388c2ea
shoya <shoyamoco021729@gmail.com>
2023-07-24 09:42:16 +0000

    いいね機能の修正

--- database/migrations/2023_06_19_022401_create_likes_table.php
+++ database/migrations/2023_06_19_022401_create_likes_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamps();
             
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('trip_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('trip_id')->references('id')->on('trips')->onDelete('cascade');
             
             $table->unique(['user_id', 'trip_id']);
         });
--- resources/views/trip/index.blade.php
+++ resources/views/trip/index.blade.php
@@ -24,32 +24,34 @@
                             <div class="row">
                                 <div class="card bg-light">
                                     <div class="card-body">
-                                        <div class="icon">
-                                            <img src="{{ secure_asset('storage/icon/icon.png') }}" alt="" height="30" width="30">
-                                            <span>{{ Auth::user()->name }}</span>
-                                            <span>
-                                                <div class="nav-item dropdown">
-                                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                                        ・・・
-                                                    </a>
-                                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                                        <a class="dropdown-item" href="{{ route('trip.edit', ['id' => $trip->id]) }}">編集</a>
-                                                        <a class="dropdown-item" href="{{ route('trip.delete', ['id' => $trip->id]) }}">削除</a>
-                                                    </div>
+                                        <div class="icon d-flex">
+                                            <div>
+                                                <img src="{{ secure_asset('storage/icon/icon.png') }}" alt="" height="30" width="30">
+                                                <span>{{ Auth::user()->name }}</span>
+                                            </div>
+                                            <div class="nav-item dropdown ms-auto">
+                                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                    ・・・
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                                    <a class="dropdown-item" href="{{ route('trip.edit', ['id' => $trip->id]) }}">編集</a>
+                                                    <a class="dropdown-item" href="{{ route('trip.delete', ['id' => $trip->id]) }}">削除</a>
                                                 </div>
-                                            </span>
+                                            </div>
                                         </div>
                                         
                                         <div class="photo">
                                             <img src="{{ secure_asset('storage/image/'. $trip->image_path) }}" alt="" height="350" width="480">
                                         </div>
-                                        <div class="icon">
-                                            <img src="{{ secure_asset('storage/icon/ハート.png') }}" alt="" height="25" width="25">
-                                            <span>
+                                        <div class="icon d-flex">
+                                            <div class="me-auto">
+                                                <img src="{{ secure_asset('storage/icon/ハート.png') }}" alt="" height="25" width="25">
+                                            </div>
+                                            <div>
         			                    	    <a href='https://www.google.com/maps/search/?api=1&query={{ $trip->map }}'>
                                                     <img src="{{ secure_asset('storage/icon/google_maps_new_logo_icon_159147.png') }}" alt="" height="25" width="25">
                                                 </a>
-        		                            </span>
+        		                            </div>
         		                        </div>
                                         <p class="card-text">
                                             <td>{{ Str::limit($trip->body, 250) }}</td>
